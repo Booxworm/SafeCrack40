1ff3af286dbf599be308f1b639cd8a9045c81f64
Wonn Jen <38198099+Booxworm@users.noreply.github.com>
2018-04-10 11:16:04 +0800

    Included some documentation

--- SafeCrack40.py
+++ SafeCrack40.py
@@ -1,5 +1,5 @@
 class Puzzle:
-    #Creating rings: inner to outer
+    # Creating rings: inner to outer
     arr1 =  [13, 0, 3, 0, 3, 0, 6, 0,10, 0,10, 0,10, 0, 6, 0]
 
     arr2 = [[10, 2, 6,10, 4, 1, 5, 5, 4, 8, 6, 3, 1, 6,10, 6],
@@ -11,6 +11,7 @@ class Puzzle:
     arr4 = [[ 9, 7, 3,12,24,10, 9,22, 9, 5,10, 5, 1,24, 2,10],
             [11,27,10,19,10,13,10, 2,15,23,19, 3, 2, 3,27,20]]
 
+    # Selects rings
     def select(self, n):
         switch = {
             1: self.arr1,
@@ -18,26 +19,22 @@ class Puzzle:
             3: self.arr3,
             4: self.arr4
         }
-        return switch.get(n,0)
+        return switch.get(n)
 
+    # Pushes a selected ring to the left
     def push(self, n):
         arr = self.select(n)
         if n == 1:
-            temp = arr[0]
-            for i in range(len(arr)-1):
-                arr[i] = arr[i+1]
-            arr[-1] = temp
-
+            temp = arr.pop(0)
+            arr.append(temp)
         else:
-            temp = [arr[0][0], arr[1][0]]
-            for i in range(len(arr[0])-1):
-                arr[0][i] = arr[0][i+1]
-                arr[1][i] = arr[1][i+1]
-            arr[0][-1] = temp[0]
-            arr[1][-1] = temp[1]
+            temp = [arr[0].pop(0), arr[1].pop(0)]
+            arr[0].append(temp[0])
+            arr[1].append(temp[1])
         return arr
 
-    def sumSingle(self, i=0, n=0):
+    # Returns the sum of a selected row i, starting from a selected ring n (defaults to outer ring)
+    def sumSingle(self, i=0, n=4):
         arr = self.select(n)
         if n == 1:
             data = arr[i]
@@ -48,6 +45,7 @@ class Puzzle:
             data = self.select(n+1)[0][i]
         return data + self.sumSingle(i, n-1)
 
+    # Prints the values of the rings
     def printP(self):
         puzzle = [
             self.arr1,
@@ -60,29 +58,34 @@ class Puzzle:
                 if puzzle[i][j] == 0:
                     puzzle[i][j] = self.select(i+2)[0][j]
 
-        print "From inner to outer:"
+        print("From inner to outer:")
         for arr in puzzle:
-            print arr
+            print(arr)
 
-        def findAns(self):
-            priCount = 0
-            secCount = 0
-            while True:
-                if self.sumSingle(0) == 40:
-                    check = 0
-                    for i in range(1,16):
-                        if not self.sumSingle(i) == 40:
-                            break
-                        else: check += 1
-                    if check == 15:
-                        self.printP()
-                        print "It took %d counts" % priCount
-                        return 0
+    # Finds the answer, including how many counts it takes
+    def findAns(self):
+        priCount = 0
+        secCount = 0
+        while True:
+            if self.sumSingle(0) == 40:
+                check = 0
+                for i in range(1,16):
+                    if not self.sumSingle(i) == 40:
+                        break
+                    else: check += 1
+                if check == 15:
+                    self.printP()
+                    print("It took %d counts" % priCount)
+                    return 0
 
-                self.push(1)
-                priCount += 1
-                if priCount % 16 == 0:
-                    self.push(2)
-                    secCount += 1
-                    if secCount % 16 == 0:
-                        self.push(3)
+            self.push(1)
+            priCount += 1
+            if priCount % 16 == 0:
+                self.push(2)
+                secCount += 1
+                if secCount % 16 == 0:
+                    self.push(3)
+
+# To find the answer
+# p = Puzzle()
+# p.findAns()
